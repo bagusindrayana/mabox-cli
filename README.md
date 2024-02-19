77e5cb2dddc3aac1b27f7872667f0c150db66043
bagusindrayana <bagusindrayanaindo@gmail.com>
2024-02-19 10:48:25 +0800

    add history for next and prev, add clear, update readme, update list link with table

--- README.md
+++ README.md
@@ -5,6 +5,17 @@ convert web menjadi teks cli
 - navigasi halaman dengan command line
 - mengambil teks dari halaman web
 
+### Command Table
+| Command | Deskripsi |
+| --- | --- |
+| help | show available commands |
+| convert | convert website to cli, example convert https://id.wikipedia.org | 
+| open | Open the links available on the website, list links ordered by sequence numbers 1,2,3 and so on |
+| clear | clear terminal |
+| next | open the next history |
+| prev | open the previous history |
+
+
 ### Kekurangan
 - hanya support navigasi dengan GET request
 - tidak support interaksi javascript
--- assets/mabox.js
+++ assets/mabox.js
@@ -2,6 +2,17 @@ let host = document.getElementById('host').innerHTML;
 const terminalDiv = document.getElementById('terminal');
 const inputLineDiv = document.getElementById('input-line');
 
+let history = [];
+let currentIndexHistory = 0;
+
+//check query sc
+const urlParams = new URLSearchParams(window.location.search);
+const sc = urlParams.get('sc');
+if(sc){
+    history.push(sc);
+    currentIndexHistory = history.length - 1;
+}
+
 function createTerminalOutput(text) {
     const outputDiv = document.createElement('div');
     outputDiv.innerHTML = text; // Use innerHTML to render HTML tags in output
@@ -12,22 +23,16 @@ function createTerminalOutput(text) {
 
 async function processCommand(input) {
     const command = input.trim();
-
-    // if (command === 'hello') {
-    //     createTerminalOutput('<span style="color: blue;">Hello there!</span>');
-    // } else if (command === 'date') {
-    //     createTerminalOutput(`<span style="color: yellow;">${new Date().toLocaleString()}</span>`);
-    // } else {
-    //     createTerminalOutput(`<span style="color: red;">Command not found: ${command}</span>`);
-    // }
-
     if(command === "help") {
         createTerminalOutput(`
             <span style="color: white;">Available commands:</span>
             <ul>
                 <li>help - show available commands</li>
                 <li>convert - convert website to cli, example convert https://id.wikipedia.org</li>
-                <li>open - Open the links available on the website, the links are opened in the sequence numbers 1,2,3 and so on</li>
+                <li>open - Open the links available on the website, list links ordered by sequence numbers 1,2,3 and so on</li>
+                <li>clear - clear terminal</li>
+                <li>next - open the next history</li>
+                <li>prev - open the previous history</li>
             </ul>
         `);
     } else if(command.startsWith("convert")) {
@@ -35,7 +40,12 @@ async function processCommand(input) {
         if(!url) {
             createTerminalOutput(`<span style="color: red;">Invalid url: ${url}</span>`);
         } else {
-            await openLink(url);
+            await openLink(url).then((v) => {
+                if(v){
+                    history.push(url);
+                    currentIndexHistory = history.length - 1;
+                }
+            });
         }
     } else if(command.startsWith("open")) {
         const links = getCurrentLinks();
@@ -47,10 +57,24 @@ async function processCommand(input) {
             if (isNaN(index) || index < 1 || index > links.length) {
                 createTerminalOutput(`<span style="color: red;">Invalid link index: ${index}</span>`);
             } else {
-                await openLink(links[index - 1]['href']);
+                const url = links[index - 1]['href'];
+                await openLink(url).then((v) => {
+                    if(v){
+                        history.push(url);
+                        currentIndexHistory = history.length - 1;
+                    }
+                });
             }
         }
-    } else {
+    } else if(command.startsWith("clear")) {
+        terminalDiv.innerHTML = "";
+    } else if(command === "next") {
+        await handleHistory("next");
+    }
+    else if(command === "prev") {
+        await handleHistory("prev");
+    }
+    else {
         createTerminalOutput(`<span style="color: red;">Command not found: ${command}</span>`);
     }
 
@@ -79,6 +103,33 @@ function handleInput(event) {
         });
         
     }
+}
+
+async function handleHistory(c){
+    if (history.length <= 0){
+        return Promise.resolve(false);
+    }
+    if (c == "prev"){
+        if (currentIndexHistory > 0){
+            currentIndexHistory--;
+        }
+        
+    } else if (c == "next") {
+        if (currentIndexHistory < history.length - 1){
+            currentIndexHistory++;
+        }
+    } else {
+        return Promise.resolve(false);
+    }
+    await openLink(history[currentIndexHistory]);
+    return Promise.resolve(true);
+}
+
+function getArgs(command) {
+    const l = command.split(' ');
+    //get args start with --
+    const args = l.filter((v) => v.startsWith('--'));
+    return args;
 }
 
 const inputField = document.getElementById('input');
@@ -118,30 +169,35 @@ async function openLink(url) {
         const response = await fetch(`/open-link?url=${url}`);
         const data = await response.json();
         if(response.status === 200) {
+            let text = data.text;
+            //replace /n with <br>
+            text = text.replace(/\n/g, "<br>");
             let output = `
             <center>
                 <pre>${ data.titleArt }</pre>
             </center>
-            <div class="textbox">${data.text}</div>`;
+            <div class="textbox">${text}</div>`;
             let linkList = "";
             if(data.links.length > 0){
-                linkList = "<p>Link : </p><ol>";
+                linkList = "<p>Link : </p><span>Use command open [link number] </span><div class='row'>";
                 const elLinks = document.getElementById('links');
                 elLinks.innerHTML = JSON.stringify(data.links);
                 host = data.host;
                 document.querySelector('#prompt span').innerHTML = host;
 
                 data.links.forEach((link, index) => {
-                    linkList += `<li>${link['text']}</li>`;
+                    linkList += `<div class="column">${index+1} - ${link['text']}</div>`;
                 });
-                linkList += "</ol>";
+                linkList += "</div>";
             }
             output += linkList;
             createTerminalOutput(output);
+            return Promise.resolve(true);
         } else {
             createTerminalOutput(`<span style="color: red;">Failed to open link: ${data.error}</span>`);
         }
     } catch (error) {
         createTerminalOutput(`<span style="color: red;">Failed to open link: ${error}</span>`);
     }
+    return Promise.resolve(false);
 }
